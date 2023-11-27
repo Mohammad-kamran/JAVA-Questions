3ba43553d96b58cd819aca72f436667fa6a2eb48
Mohammad kamran <mdkdev9@gmail.com>
2023-11-27 18:04:55 +0530

    File is modified after push

--- src/Array_ArrayList/intro.java
+++ src/Array_ArrayList/intro.java
@@ -1,4 +1,13 @@
 package Array_ArrayList;
-
+//Array a collection of data type
 public class intro {
+    public static void main(String[] args) {
+        // Declaration of array
+        int[] arr ; // arr defined in stack memory
+
+        //Initialization
+        arr = new int[10]; // here object is being created in heap memory
+
+
+    }
 }
