5c9ccb0c077251fd777bea1805885751395ff690
Mohammad kamran <mdkdev9@gmail.com>
2023-11-27 20:38:36 +0530

    Mutiblity in java arrays

---
+++ src/Array_ArrayList/PassingInFunction.java
@@ -0,0 +1,20 @@
+package Array_ArrayList;
+
+import java.util.Arrays;
+
+public class PassingInFunction {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        System.out.println(Arrays.toString(nums));
+
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+
+
+    }
+    //That  phenomena are  called mutability in java
+
+    public static void change(int[] arr){
+        arr[0] = 999;
+    }
+}
