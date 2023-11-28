81edc1443f7f6da261d461bdf4b0a5ae865d3869
Mohammad kamran <mdkdev9@gmail.com>
2023-11-28 16:40:22 +0530

    2D array

---
+++ src/Arrays2D/Arr2D.java
@@ -0,0 +1,33 @@
+package Arrays2D;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Arr2D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][3] ;
+//        System.out.println(arr.length);
+        //taking input in 2D array
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = sc.nextInt();
+            }
+        }
+
+        //printing output
+//        for(int row = 0 ; row < arr.length ; row ++){
+//            for(int col = 0 ; col < arr[row].length; col++){
+//                System.out.print(arr[row][col] + "  ");
+//            }
+//            System.out.println();
+//        }
+
+        //printing output
+        for(int row = 0 ; row < arr.length ; row ++){
+            System.out.println(Arrays.toString(arr[row]));
+        }
+    }
+
+}
