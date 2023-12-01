e219cda216853efeea1a034596fda9d2bbf2a958
Mohammad kamran <mdkdev9@gmail.com>
2023-12-01 17:32:21 +0530

    Search in pwerticular range

---
+++ src/Searching/Searchinrange.java
@@ -0,0 +1,29 @@
+package Searching;
+
+import java.util.Scanner;
+
+public class Searchinrange {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] array = {12,2,23,34,45,56,56,67,78,854,35,34};
+        System.out.println("Enter the range ");
+        int start = input.nextInt();
+        int end = input.nextInt();
+        System.out.println("Enter target ");
+        int target = input.nextInt();
+        int result = SearchInRange(array,start,end,target);
+        System.out.println(result);
+
+
+    }
+    static int SearchInRange(int[] array , int start , int end , int target ){
+        for (int i = start; i <= end ; i++) {
+            int element = array[i];
+            if(element == target){
+                return array[i];
+            }
+        }
+        return -1;
+    }
+}
