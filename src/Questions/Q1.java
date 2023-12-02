8a103246920e55ae3f877a910bfa663aa10f1333
Mohammad kamran <mdkdev9@gmail.com>
2023-12-02 23:00:17 +0530

    modified for case check :

--- src/Questions/Q1.java
+++ src/Questions/Q1.java
@@ -5,6 +5,7 @@ public class Q1 {
         //Find how many numbers are there contains even number of digits
         int[] nums = {12,234,123,3,2344,2,244};
         System.out.println(findNumbers(nums));
+        System.out.println(digits(0));
     }
     static int findNumbers(int[] nums) {
         int cout = 0;
@@ -23,6 +24,14 @@ public class Q1 {
     }
     //check number of digits
     static int digits (int num) {
+        //check if number is negative make it positive
+        if(num < 0) {
+           num =  num * -1;
+        }
+        // case check
+        if(num == 0) {
+            return 1;
+        }
         int digit = 0;
         while (num > 0) {
 
