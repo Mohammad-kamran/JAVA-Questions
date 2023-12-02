804487ded1c0a0f7ea295a565082bedb50ba8921
Mohammad kamran <mdkdev9@gmail.com>
2023-12-02 22:36:41 +0530

    mofied

--- src/Questions/Q1.java
+++ src/Questions/Q1.java
@@ -1,12 +1,12 @@
 package Questions;
 
 public class Q1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Find number in even number of digits  Leetcode 1295
         int[] nums = {12,234,123,3,2344,2,244};
         System.out.println(findNumbers(nums));
     }
-    static int findNumbers(int[] nums){
+    static int findNumbers(int[] nums) {
         int cout = 0;
         for (int num : nums) {
             if(even(num)){
@@ -16,17 +16,15 @@ public class Q1 {
         return cout;
     }
 
-    static boolean even(int num){
-        int Numberofdigits = digits(num);
-        if (Numberofdigits % 2 == 0 ){
-            return true;
-        }
-        return false;
+    //check Number of digits is even
+    static boolean even(int num) {
+        int NumberOfDigits = digits(num);
+        return NumberOfDigits % 2 == 0;
     }
-
-    static int digits (int num){
+    //check number of digits
+    static int digits (int num) {
         int digit = 0;
-        while (num > 0){
+        while (num > 0) {
 
             digit++;
             num = num /10;
